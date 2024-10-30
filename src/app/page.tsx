13b85763644e599f1379716946d79a068dcabcf0
Kohla <f.kohla@gopus.de>
2024-10-30 08:50:41 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 import PlantTrackerPage from './plant-tracker/page'
 
 export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (!token) {
-      router.push('/login')
-    }
-  }, [router])
-
   return <PlantTrackerPage />
 }
